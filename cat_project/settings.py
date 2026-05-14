962df2d6033cb1e5708e98b3e7885025b3122f79
WilsonPsychedelic <michaelbrooks452@gmail.com>
2026-05-13 19:22:17 -0700

    Modified settings.py for auth token

--- cat_project/settings.py
+++ cat_project/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
+    'rest_framework.authtoken',
     'cats',
 ]
 
@@ -117,3 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ],
+}
